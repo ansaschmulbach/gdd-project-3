c4cf5b66f2386727236de9c1d7b83f557900ecf5
Viansa Schmulbach <ansa@berkeley.edu>
2021-04-07 10:29:21 -0700

    more intuitive movement

---
+++ GDD Project 3/Assets/Ansa Player Movement/AnsaPlayerMovement.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnsaPlayerMovement : PlayerMovement
+{
+
+    [Header("Jump variables")]
+    [SerializeField] private float timeInAir;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float xJumpDist;
+    
+    [Header("Land variables")]
+    [SerializeField] private float x0Land; //x(0) velocity on land
+    [SerializeField] private float xAccLand;
+    
+    
+    #region Calculated Variables
+
+    private float xJumpVel;
+    private float yJumpV0;
+    private float gravity;
+
+    #endregion
+    
+    #region Private Variables
+
+    [SerializeField] private Vector3 velocity;
+    [SerializeField] private Vector3 acceleration;
+    [SerializeField] private bool isJumping;
+    private bool isTouchingGround;
+    private bool isJumpingLast; //used to detect a switch in jumping
+    [SerializeField] private bool firstXPress = true;
+
+    #endregion
+
+    void Start()
+    {
+        xJumpVel = XVel();
+        yJumpV0 = JumpY0Vel();
+        gravity = Gravity();
+        this.velocity = Vector3.zero;
+        this.acceleration = Vector3.up * gravity;
+        isJumping = false;
+    }
+
+    void Update()
+    {
+        
+        xJumpVel = XVel();
+        yJumpV0 = JumpY0Vel();
+        gravity = Gravity();
+        
+        if (Input.GetKeyDown("space"))
+        {
+            this.velocity.y = yJumpV0;
+            this.velocity.x = 0;
+            isJumping = true;
+            isTouchingGround = false;
+        }
+        
+        float xInput = Input.GetAxis("Horizontal");
+        float xMult = firstXPress ? 1 : 0;
+        float xVelAdd = isJumping ? xJumpVel : x0Land;
+        firstXPress = !firstXPress && (xInput != 0);
+        velocity.x += xVelAdd * xMult;
+        velocity.x *= xInput;
+        velocity.z = 0;
+
+
+        float xAccel = isJumping ? 0 : xAccLand;
+        acceleration.x = xAccel;
+        acceleration.y = gravity;
+        acceleration.z = 0;
+        Debug.Log("VelAccel: " +  velocity + ", " + acceleration);
+        
+        UpdatePositionVelocity();
+    }
+
+    #region Position Update methods
+
+    void UpdatePositionVelocity()
+    {
+        this.transform.position += velocity * Time.deltaTime + (0.5f * Time.deltaTime * Time.deltaTime) * acceleration;
+        this.velocity += acceleration * Time.deltaTime;
+        if (isTouchingGround)
+        {
+            this.velocity.y = 0;
+        }
+    }
+
+    #endregion
+
+    #region Calculating Variables Methods
+
+    float XVel()
+    {
+        return xJumpDist / timeInAir;
+    }
+
+    float JumpY0Vel()
+    {
+        return (2 * jumpHeight)/timeInAir;
+    }
+
+    float Gravity()
+    {
+        return (-2 * jumpHeight) / (timeInAir * timeInAir);
+    }
+
+    #endregion
+
+    #region Collision methods
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+            isTouchingGround = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            isTouchingGround = false;
+        }
+    }
+    
+
+    #endregion
+
+}
--- GDD Project 3/Assets/Player Movement/Scripts/PlayerMovement.cs
+++ GDD Project 3/Assets/Player Movement/Scripts/PlayerMovement.cs
@@ -7,18 +7,18 @@ public class PlayerMovement : MonoBehaviour
 {
 
 	#region Movement variables
-
+	
 	[SerializeField]
 	[Tooltip("Movement speed")]
 	public float movementSpeed;
-
+	
 	[SerializeField]
 	[Tooltip("Can you move mid-air?")]
 	public bool touchingFloor;
-
+	
 	[SerializeField] [Tooltip("Can you wall jump?")]
 	public bool canWallJump;
-
+	
 	[SerializeField]
 	[Tooltip("Jump speed")]
 	public float jumpSpeed;
@@ -31,41 +31,41 @@ public class PlayerMovement : MonoBehaviour
 	private bool canDoubleJump;
 	
 	#endregion
-
+ 
 	#region Jumping variables
-
+ 
 	private float jumpTimer;
-
+ 
 	[SerializeField]
 	[Tooltip("Max jump rate in seconds")]
 	private float maxJumpRate = 0.6f;
-
+ 
 	private bool doubleJumpThrottle;
-
+ 
 	/**If you're touching a wall, you can't propulse*/
 	private bool touchingWall;
-
+ 
 	private Vector2 jumpVector;
 	private Vector2 pushVector;
-
+ 
 	private Vector2 vel;
-
+ 
 	private bool hasDoubleJumped = true;
-
+ 
 	#endregion
-
+ 
 	#region Propulsion variables
-
+ 
 	private bool m_canPropulse = true;
 	public bool canPropulse
 	{
 		get { return m_canPropulse; }
 		set { m_canPropulse = value; }
 	}
-
+ 
 	/* Player controller. */
 	private PlayerController pc;
-
+ 
 	/* Whether the player is touching a wall on their left. */
 	private bool left;
 	public bool leftContact
@@ -75,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
 			left = value;
 		}
 	}
-
+ 
 	/* Whether the player is touching a wall on their right. */
 	private bool right;
 	public bool rightContact
@@ -85,139 +85,140 @@ public class PlayerMovement : MonoBehaviour
 			right = value;
 		}
 	}
-
+ 
 	#endregion
-
-	#region Player Components
-
-	private Rigidbody2D rb;
-
-	private AudioSource asrc;
-
-	#endregion
-
-	#region Unity methods
-
-	void Start()
-    {
-		rb = GetComponent<Rigidbody2D>();
-		rb.interpolation = RigidbodyInterpolation2D.Interpolate;
-		pc = GetComponent<PlayerController>();
-		//asrc = GetComponent<AudioSource>();
-		//asrc.playOnAwake = false;
-		jumpTimer = 0;
-		jumpVector = new Vector2(0, jumpSpeed/32);
-		pushVector = new Vector2(jumpSpeed / 36, 0);
-		vel = Vector2.zero;
-		left = false;
-		right = false;
-	}
-
-	void Update()
-    {
-
-		if (!isActive)
-		{
-			return;
-		}
-
-		if (Input.GetKey(KeyCode.Space))
-		{
-			Jump();
-		} else
-		{
-			doubleJumpThrottle = false;
-		}
-
-		if (m_canPropulse || touchingFloor)
-		{
-
-			float xDir = Input.GetAxisRaw("Horizontal");
-			xDir *= 0.2f;
-			vel = rb.velocity;
-
-			if (left || right)
-			{
-				if (left && right && touchingFloor)
-                {
-					//pc.Die();
-                }
-				if (xDir != 0) { 
-					vel.y = Mathf.Max(-1f, vel.y);
-				}
-			}
-			vel.x = xDir + Mathf.Lerp(vel.x, xDir * movementSpeed, 0.02f);
-			rb.velocity = vel;
-			
-		}
-
-		jumpTimer = Mathf.Max(0f, jumpTimer - Time.deltaTime);
-
-	}
-
-	#endregion
-
-	#region Jump method
-
-	private void Jump()
-	{
-
-		if (jumpTimer == 0)
-		{
-			jumpTimer = maxJumpRate;
-
-
-			if (touchingFloor)
-			{
-				Debug.Log("JUMP");
-				// rb.AddForce(Vector2.up * jumpSpeed);
-				rb.velocity += jumpVector;
-				transform.localPosition += new Vector3(0, 1f, 0);
-				doubleJumpThrottle = true;
-			}
-			else if (left)
-            {
-				// rb.AddForce(new Vector2(jumpSpeed, jumpSpeed));
-				rb.velocity += jumpVector + pushVector;
-
-			}
-			else if (right)
-            {
-	            // rb.AddForce(new Vector2(-jumpSpeed, jumpSpeed));
-				rb.velocity += jumpVector - pushVector;
-			} 
-			else if (!hasDoubleJumped && canDoubleJump && !doubleJumpThrottle)
-			{
-				//rb.velocity = new Vector2(rb.velocity.x, 0);
-				Debug.Log("DOUBLE JUMP");
-				rb.velocity += jumpVector;
-				transform.localPosition += new Vector3(0, 1f, 0);
-				hasDoubleJumped = true;
-			}
-
-			//asrc.Play();
-		}
-	}
-
-	#endregion
-
-	#region Collision Methods
-
-	private void OnCollisionEnter2D(Collision2D other)
-	{
-		if (other.collider.CompareTag("Floor"))
-		{
-			hasDoubleJumped = false;
-		}
-	}
-
-	// private void OnCollisionExit2D(Collision2D other)
-	// {
-	// 	if (other.collider.CompareTag("Wall")) 
+	
+ //
+	// #region Player Components
+ //
+	// private Rigidbody2D rb;
+ //
+	// private AudioSource asrc;
+ //
+	// #endregion
+ //
+	// #region Unity methods
+ //
+	// void Start()
+ //    {
+	// 	rb = GetComponent<Rigidbody2D>();
+	// 	rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+	// 	pc = GetComponent<PlayerController>();
+	// 	//asrc = GetComponent<AudioSource>();
+	// 	//asrc.playOnAwake = false;
+	// 	jumpTimer = 0;
+	// 	jumpVector = new Vector2(0, jumpSpeed/32);
+	// 	pushVector = new Vector2(jumpSpeed / 36, 0);
+	// 	vel = Vector2.zero;
+	// 	left = false;
+	// 	right = false;
+	// }
+ //
+	// void Update()
+ //    {
+ //
+	// 	if (!isActive)
 	// 	{
+	// 		return;
+	// 	}
+ //
+	// 	if (Input.GetKey(KeyCode.Space))
+	// 	{
+	// 		Jump();
+	// 	} else
+	// 	{
+	// 		doubleJumpThrottle = false;
+	// 	}
+ //
+	// 	if (m_canPropulse || touchingFloor)
+	// 	{
+ //
+	// 		float xDir = Input.GetAxisRaw("Horizontal");
+	// 		xDir *= 0.2f;
+	// 		vel = rb.velocity;
+ //
+	// 		if (left || right)
+	// 		{
+	// 			if (left && right && touchingFloor)
+ //                {
+	// 				//pc.Die();
+ //                }
+	// 			if (xDir != 0) { 
+	// 				vel.y = Mathf.Max(-1f, vel.y);
+	// 			}
+	// 		}
+	// 		vel.x = xDir + Mathf.Lerp(vel.x, xDir * movementSpeed, 0.02f);
+	// 		rb.velocity = vel;
 	// 		
 	// 	}
+ //
+	// 	jumpTimer = Mathf.Max(0f, jumpTimer - Time.deltaTime);
+ //
 	// }
+ //
+	// #endregion
+ //
+	// #region Jump method
+ //
+	// private void Jump()
+	// {
+ //
+	// 	if (jumpTimer == 0)
+	// 	{
+	// 		jumpTimer = maxJumpRate;
+ //
+ //
+	// 		if (touchingFloor)
+	// 		{
+	// 			Debug.Log("JUMP");
+	// 			// rb.AddForce(Vector2.up * jumpSpeed);
+	// 			rb.velocity += jumpVector;
+	// 			transform.localPosition += new Vector3(0, 1f, 0);
+	// 			doubleJumpThrottle = true;
+	// 		}
+	// 		else if (left)
+ //            {
+	// 			// rb.AddForce(new Vector2(jumpSpeed, jumpSpeed));
+	// 			rb.velocity += jumpVector + pushVector;
+ //
+	// 		}
+	// 		else if (right)
+ //            {
+	//             // rb.AddForce(new Vector2(-jumpSpeed, jumpSpeed));
+	// 			rb.velocity += jumpVector - pushVector;
+	// 		} 
+	// 		else if (!hasDoubleJumped && canDoubleJump && !doubleJumpThrottle)
+	// 		{
+	// 			//rb.velocity = new Vector2(rb.velocity.x, 0);
+	// 			Debug.Log("DOUBLE JUMP");
+	// 			rb.velocity += jumpVector;
+	// 			transform.localPosition += new Vector3(0, 1f, 0);
+	// 			hasDoubleJumped = true;
+	// 		}
+ //
+	// 		//asrc.Play();
+	// 	}
+	// }
+ //
+	// #endregion
+ //
+	// #region Collision Methods
+ //
+	// private void OnCollisionEnter2D(Collision2D other)
+	// {
+	// 	if (other.collider.CompareTag("Floor"))
+	// 	{
+	// 		hasDoubleJumped = false;
+	// 	}
+	// }
+ //
+	// // private void OnCollisionExit2D(Collision2D other)
+	// // {
+	// // 	if (other.collider.CompareTag("Wall")) 
+	// // 	{
+	// // 		
+	// // 	}
+	// // }
 
-	#endregion
+	//#endregion
 }
